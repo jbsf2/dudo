c5b6ed1a62c23758e0950482a27a6a50ee73c64e
jb <jb@foo.com>
2020-09-06 11:18:12 -0700

    fetching players by id

--- lib/dudo/players.ex
+++ lib/dudo/players.ex
@@ -5,8 +5,7 @@ defmodule Dudo.Players do
   def create_player(attrs), do: %Player{} |> change_player(attrs) |> Repo.insert()
   def change_player(%Player{} = player, attrs), do: Player.changeset(player, attrs)
   def list_players(), do: Player.Query.all() |> Repo.all()
-
-  #  def get_user(id), do: User |> Repo.get(id)
+  def get_player(id), do: Player |> Repo.get(id)
   #  def create_user!(attrs), do: %User{} |> change_user(attrs) |> Repo.insert!()
 
 end
--- test/dudo/players_test.exs
+++ test/dudo/players_test.exs
@@ -2,12 +2,21 @@ defmodule Dudo.PlayersTest do
   use Dudo.DataCase, async: true
 
   alias Dudo.Players
-  alias Dudo.Test
 
   test "create_player creates a player" do
     {:ok, player} = %{name: "Player 1"} |> Players.create_player()
 
     assert player.name == "Player 1"
+  end
+
+  test "get_player finds an existing player" do
+    {:ok, player} = %{name: "Player 1"} |> Players.create_player()
+
+    fetched_player = Players.get_player(player.id)
+
+    assert fetched_player.id == player.id
+    assert fetched_player.name == player.name
+
   end
 
 #  test "create_user! creates a user" do
