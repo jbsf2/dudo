aca00ecc1fcce4117693271f4fd71b8678ce0af2
jb <jb@foo.com>
2020-12-17 23:18:04 -0800

    trying to deploy

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,7 @@ config :dudo, DudoWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "dudoweb.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
